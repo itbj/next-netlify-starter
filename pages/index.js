3320afda69b1d613a5cc98294d7f9b3b0786cd20
taoza <itbj00@163.com>
2022-05-26 20:27:54 +0800

    Update index.js
    anatomical heart

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app! anatomical skunk" />
+        <Header title="Welcome to my app! anatomical heart" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
