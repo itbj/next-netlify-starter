f06dc52857fbd9079e82b62d2f11e303d3e650a6
taoza <itbj00@163.com>
2022-05-28 16:28:42 +0800

    goto cisco.com

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,12 @@ export default function Home() {
         <p className="description">
           Get started by editing <code>pages/index.js</code>12345
         </p>
+          <h1>First Post</h1>
+      <h2>
+        <Link href="www.cisco.com">
+          <a>Goto cisco.com</a>
+        </Link>
+      </h2>
       </main>
 
       <Footer />
