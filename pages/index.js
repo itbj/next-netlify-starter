99eada9a6311d3f55d2ead5d0cd1c9319589d136
taoza <itbj00@163.com>
2022-05-28 13:10:04 +0800

    Update index.js
    12345

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
         <Header title="Welcome to my app! stoat" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
-    <br>1234.
+    <br>12345
         </p>
       </main>
 
